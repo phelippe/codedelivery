cdf5577368c462dd8f14bb7347bc4e5177e8b1df
Phelippe Matte <phelippematte@gmail.com>
2015-09-11 09:38:59 -0300

    inicio

---
+++ database/seeds/ClientTableSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use CodeDelivery\Models\Category;
+use CodeDelivery\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ClientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        #feito no UserTableSeeder
+    }
+}
