0d050eea58a10ace04db7131a04d23a202afe8c4
Phelippe Matte <phelippematte@gmail.com>
2015-11-13 11:05:51 -0300

    .

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -3,17 +3,52 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/home/:nome',
+                templateUrl: 'templates/home.html',
+                controller: 'HomeController',
+                /*controller: function($scope, $state, $stateParams){
+                    $scope.state = $state.current;
+                    $scope.nome = $stateParams.nome;
+                },*/
+            })
+            .state('home.a', {
+                url: '/a',
+                templateUrl: 'templates/home-a.html',
+            })
+            .state('home.b', {
+                url: '/b',
+                templateUrl: 'templates/home-b.html',
+            })
+            .state('main', {
+                url: '/',
+                templateUrl: 'templates/main.html',
+            })
+            /*.state('main.a', {
+                url: '/a',
+                templateUrl: 'templates/main-a.html',
+            })
+            .state('main.b', {
+                url: '/b',
+                templateUrl: 'templates/main-b.html',
+            })*/;
+
+        $urlRouterProvider.otherwise('/');
+    });
